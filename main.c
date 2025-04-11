84b37cee892a44bc7c52d10f395b5f779ff38f65
Nawfal <mahyo.nawfal@gmail.com>
2025-04-11 16:57:15 +0200

    test1

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "structure.h"
 
-int main(){
-    printf("fnn");
+
+
+int main(int argc, char* argv[]){
+    Couronne couronne;
+    printf("Saisir le nombre de points de vie de la couronne\n");
+    if (scanf("%d", &couronne.pv) != 1 || couronne.pv <= 0){
+        printf("Erreur de saisie\n"); //a fixer (boucle)
+        return 1;
+    } 
+    
 }
---
+++ structure.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+typedef struct{
+    int x;
+    int y;
+    int pv;
+}Crabe;
+
+typedef struct{
+    int x;
+    int y;
+    int degat;
+    int rayon;
+}Singe;
+
+typedef struct{
+    int x;
+    int y;
+    int pv;
+}Couronne;
